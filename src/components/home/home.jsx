d7c3496484f64b384d46be692479faa8758430b2
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-05-08 11:54:17 +0530

    container3 done

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import school from '../../assets/school.jpg'; // Corrected import statement for the school image
 import Slider from './slider';
+import cont3img1 from '../../assets/cont3img1.png';
+import cont3img2 from '../../assets/cont3img2.png';
+import cont3img3 from '../../assets/cont3img3.png';
 function Home() {
     return (
         <div>
@@ -15,6 +18,29 @@ function Home() {
                 <div>
                     <Slider />
                 </div>
+            </div>
+            <div 
+                className='h-lvh flex justify-center items-center gap-28'
+                style={{
+                    backgroundImage: 'linear-gradient(to bottom, #081F37 3%, #52D3D8 100%)',
+                }}
+            >
+                <Programs image={cont3img1} content="Our educators blend academic expertise with real-world insights,  cultivating a dynamic learning environment. They inspire, engage, and  empathize with students, nurturing their individuality and aspirations.  With advanced degrees and a passion for teaching, they personalize  instruction to meet diverse learning styles"/>
+                <Programs image={cont3img2} content="We blend child-led exploration, group learning, and guided skill  development to achieve our children's goals. Our seamless daily schedule  integrates indoor and outdoor activities, fostering comprehension  across diverse concepts. Specialized sessions in dedicated areas further  enrich their learning experiences." />
+                <Programs image={cont3img3} content="Our program nurtures students' skills in fine art, dance, and drama,  fostering confidence, creativity, and teamwork. Core values of kindness,  respect, and integrity define our ethos, where mutual support and  celebration of achievements are paramount. Each student is valued and  expected to extend respect to peers, forming a supportive community." />                
+            </div>
+        </div>
+    );
+}
+
+function Programs(props) {
+    return (
+        <div className='flex flex-col justify-center items-center max-w-80 h-[568px]'>
+            <div>
+                <img src={props.image} alt="" height="300"/>
+            </div>
+            <div className='text-center text-white pt-12'>
+                <p>{props.content}</p>
             </div>
         </div>
     );
