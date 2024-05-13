131e316686d367ce44703051efe67096373cb896
Jaswanth <Jaswanth7082@gmail.com>
2024-05-13 23:52:10 +0530

    modified gallery page

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { imageDb } from '../../config/firebase-config';
+import { getDownloadURL, listAll, ref } from "firebase/storage";
+import Footer from "../Footer/Footer";
 
 const Gallery = () => {
+    const [imgUrl, setImgUrl] = useState([]);
+
+    useEffect(() => {
+        listAll(ref(imageDb, "files")).then(imgs => {
+            const promises = imgs.items.map(item =>
+                getDownloadURL(item).then(url => url)
+            );
+            Promise.all(promises).then(urls => {
+                // Filter out duplicate URLs
+                const uniqueUrls = Array.from(new Set(urls));
+                // Reverse the array to show latest uploads at the top
+                setImgUrl(uniqueUrls.reverse());
+            });
+        });
+    }, []);
+
     return (
         <div>
-            {/* Your gallery content goes here */}
+        <div className="container mx-auto px-4 py-8 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {imgUrl.map((imageUrl, index) => (
+                <div key={index} className="rounded-lg overflow-hidden shadow-md">
+                    <img
+                        src={imageUrl}
+                        alt={`Image ${index}`}
+                        className="w-full h-64 object-contain"
+                    />
+                </div>
+            ))}
+            
+        </div>
+        <Footer />
         </div>
     );
 };
