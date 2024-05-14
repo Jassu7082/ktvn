458ff4170ba144500b934a0597aa2163af4c852d
Jaswanth <Jaswanth7082@gmail.com>
2024-05-14 14:11:14 +0530

    loading bar for gallery

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -5,6 +5,7 @@ import Footer from "../Footer/Footer";
 
 const Gallery = () => {
     const [imgUrl, setImgUrl] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // State to track loading status
 
     useEffect(() => {
         listAll(ref(imageDb, "files")).then(imgs => {
@@ -16,25 +17,34 @@ const Gallery = () => {
                 const uniqueUrls = Array.from(new Set(urls));
                 // Reverse the array to show latest uploads at the top
                 setImgUrl(uniqueUrls.reverse());
+                setIsLoading(false); // Set loading to false after images are loaded
             });
         });
     }, []);
 
+    // Render loading spinner or message if still loading
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>
+            </div>
+        );
+    }
+
     return (
         <div>
-        <div className="container mx-auto px-4 py-8 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {imgUrl.map((imageUrl, index) => (
-                <div key={index} className="rounded-lg overflow-hidden shadow-md">
-                    <img
-                        src={imageUrl}
-                        alt={`Image ${index}`}
-                        className="w-full h-64 object-contain"
-                    />
-                </div>
-            ))}
-            
-        </div>
-        <Footer />
+            <div className="container mx-auto px-4 py-8 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {imgUrl.map((imageUrl, index) => (
+                    <div key={index} className="rounded-lg overflow-hidden shadow-md">
+                        <img
+                            src={imageUrl}
+                            alt={`Image ${index}`}
+                            className="w-full h-64 object-contain"
+                        />
+                    </div>
+                ))}
+            </div>
+            <Footer />
         </div>
     );
 };
