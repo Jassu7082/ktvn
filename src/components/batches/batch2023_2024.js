66e6942a62215e5d248486482f76de0b7200c3f9
Jaswanth <Jaswanth7082@gmail.com>
2024-05-29 17:18:54 +0530

    batch 2023-2024 added

--- src/components/batches/batch2023_2024.js
+++ src/components/batches/batch2023_2024.js
@@ -3,19 +3,198 @@ const batch2023 = [
       batch: '2023',
       name: ' Y Nitya Sree',
       marks: "594",
-      imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fnityasree.jpg?alt=media&token=b60a44bb-ad7e-482c-9a78-efde1436bc0f", // Placeholder image URL
+      imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fnityasree.jpeg?alt=media&token=713b8dac-a139-4e58-b271-2da7c6c54eff", // Placeholder image URL
     },
     {
         batch: '2023',
         name: 'B Anusha',
         marks: "592",
-        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fanusha.jpg?alt=media&token=eb91aa48-fa74-4232-b58c-09837e37e12f", // Placeholder image URL
+        imageUrl:"https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fanusha.jpeg?alt=media&token=08540212-1397-4731-8852-3003eea725fe", // Placeholder image URL
       }, {
         batch: '2023',
-        name: 'Tarun Kumar',
+        name: 'C Tarun Kumar Reddy',
         marks: "582",
-        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Ftarun.jpg?alt=media&token=f7b047d9-12b5-46aa-81b7-051a840387ad", // Placeholder image URL
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Ftarun.jpeg?alt=media&token=89781587-5fb6-4e2f-ad14-d2c8daf76a93", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' K Gnanaroopa',
+        marks: "578",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fgnanarupa.jpeg?alt=media&token=989109ff-2527-4d98-8cf2-62a7dced56a1", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' M Harshihavardhan Reddy',
+        marks: "576",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fharshavardhan.jpeg?alt=media&token=52c20e17-64b6-4db6-82a7-1ecf17c0fbc5", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' B Jeevan',
+        marks: "569",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fjeevan.jpeg?alt=media&token=8bdaac5c-cb4c-4549-9b50-8bc9c5f2832c", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: 'P Akshara',
+        marks: "569",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fakshara.jpeg?alt=media&token=a77e0064-27ed-4c03-8e72-bfb1b21d42ab", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' P Nikhileswari',
+        marks: "564",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fnikhileswari.jpeg?alt=media&token=23bef56e-89fc-47eb-a2a9-5d0c5589579e", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' O Mohan Reddy',
+        marks: "562",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fmohan.jpeg?alt=media&token=d487bea5-ca33-4ce0-93e6-de9841869e2b", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' D Charan Teja',
+        marks: "558",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fcharan.jpeg?alt=media&token=dad46bb6-26d3-43d9-a941-babc39c8e283", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' S Kavya Sree',
+        marks: "557",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fkavya.jpeg?alt=media&token=a4a7b95c-1c6e-4787-adfd-ebeca6f9340a", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' B Uday Kiran',
+        marks: "556",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fuday.jpeg?alt=media&token=8379b9c6-7da9-4d65-b4fe-0fe9577f0584", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' P Ganapath Kumar',
+        marks: "554",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fganapati.jpeg?alt=media&token=3badec95-fd1f-4dec-9d94-4bb7bbc5e8c5", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' C Govardhan Reddy',
+        marks: "552",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fgoverdhan.jpeg?alt=media&token=dc10c5a9-2f03-4c9e-8c7d-043787341120", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' S Roshini',
+        marks: "550",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Froshini.jpeg?alt=media&token=2df052dd-a807-4525-b74d-98b231b43cdd", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' M Yuvakishor',
+        marks: "550",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fhimakishore.jpeg?alt=media&token=8fd818d9-c734-452f-9f2a-190a4d355ea7", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' B Vamsi Krishna',
+        marks: "547",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fvamsi.jpeg?alt=media&token=be43e97d-e42e-4ce6-8858-8e2206567e78", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' K Girivardhan Reddy',
+        marks: "546",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fgiri.jpeg?alt=media&token=c1206e9a-298e-4ed5-9264-0ffc6f7f8228", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: 'P Sairam',
+        marks: "540",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fsairam.jpeg?alt=media&token=b93bb7e0-f531-4872-b15f-f9edb6a6711a", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: 'O Rekha',
+        marks: "540",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Frekha.jpeg?alt=media&token=722121b0-0186-456d-be88-ca6ac3726265", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' B Sai Vikas Naidu',
+        marks: "530",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fsaivikas.jpeg?alt=media&token=7c2e6d55-1489-498c-8734-f32c438e1e23", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: 'K Vijay Kumar',
+        marks: "529",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fvijay.jpeg?alt=media&token=2f6def39-a1bd-4195-8895-e74d5911499e", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' C Arun Kumar',
+        marks: "527",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Farun.jpeg?alt=media&token=2d5318e1-e1d2-4098-aeb7-26cef4a15f33", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' K Abhiram Reddy',
+        marks: "527",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fabhiram.jpeg?alt=media&token=f5886cbd-6e7c-40eb-8141-2d5ca0417535", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' B Shivaiah',
+        marks: "526",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fshiva.jpeg?alt=media&token=db85440a-156f-48fd-b38f-310b0fe81e6c", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' M Revanth Naidu',
+        marks: "526",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Frevanth.jpeg?alt=media&token=5c14677b-970a-4d53-b3e5-6a9f544b41a8", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' M Anwar Basha',
+        marks: "526",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fanvar.jpeg?alt=media&token=5cc568d2-4740-4050-af74-ecd26333dc48", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' K Maheswari',
+        marks: "526",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fmaheswari.jpeg?alt=media&token=5c46a802-108d-4140-93df-766073a65aa0", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' A Sai Bhavani Prasad',
+        marks: "525",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fbhavani.jpeg?alt=media&token=c0cd950b-3a51-440b-a65f-4eb524ea3fc0", // Placeholder image URL
+      },
+      {
+        batch: '2023',
+        name: ' G Susmitha',
+        marks: "507",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fsusmitha.jpeg?alt=media&token=18b2e7ca-d055-44e4-bd4e-e91f190f9fd8", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' P Jayanth',
+        marks: "506",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fjayanth.jpeg?alt=media&token=581bd663-5e16-4eb2-873b-93213b1757b3", // Placeholder image URL
+      }, 
+      {
+        batch: '2023',
+        name: ' B Nandini',
+        marks: "504",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fnandini.jpeg?alt=media&token=3e33b608-e4f2-4962-92ea-95a5e79c901b", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' B Bhagya sree',
+        marks: "495",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fbhagyasree.jpeg?alt=media&token=25578fef-1b9b-4a02-8aeb-5871982e2e23", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' P Yathish Kumar Reddy',
+        marks: "495",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Fetishish.jpeg?alt=media&token=88a64a68-8f6b-410b-8f9d-a3aa9761f0e4", // Placeholder image URL
+      }, {
+        batch: '2023',
+        name: ' B Rishitha',
+        marks: "494",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/kakatiya-98486.appspot.com/o/batches%2F2023-2024%2Frishitha.jpeg?alt=media&token=b4ff88eb-98d3-4799-84d0-82082ab099de", // Placeholder image URL
       }
+      // , {
+      //   batch: '2023',
+      //   name: ' Y Nitya Sree',
+      //   marks: "594",
+      //   imageUrl: "", // Placeholder image URL
+      // }
   ];
 
   export default batch2023;
